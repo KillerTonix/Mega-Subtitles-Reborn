59d7c0edb4548c02a60c63af27ec64efc8b08db7
Artur <Artur@DESKTOP-I9VM6LM>
2025-04-23 10:18:50 +0400

    Add project files.

---
+++ Mega Subtitles Reborn/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Mega_Subtitles_Reborn
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
