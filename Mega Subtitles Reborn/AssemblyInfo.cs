338e55dd0eea39fb8915def41dc4e18c0cddaa07
Artur <Artur@DESKTOP-I9VM6LM>
2025-06-19 15:44:15 +0400

    change to 0.1.2.0

--- Mega Subtitles Reborn/AssemblyInfo.cs
+++ Mega Subtitles Reborn/AssemblyInfo.cs
@@ -18,6 +18,6 @@ using System.Windows;
 [assembly: AssemblyTrademark("Artur Tonoyan")] // Optional
 [assembly: AssemblyCulture("")] // Optional
 
-[assembly: AssemblyFileVersion("0.1.3.0")]
-[assembly: AssemblyVersion("0.1.3.0")]
+[assembly: AssemblyFileVersion("0.1.2.0")]
+[assembly: AssemblyVersion("0.1.2.0")]
 
